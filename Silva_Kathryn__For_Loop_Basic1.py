7865946d8e14dea7d82355ac210e92f9121697a1
Kathryn <kathrynsilvaconway@yahoo.com>
2021-06-14 19:53:21 -0700

    First Python loop exercise.

---
+++ Silva_Kathryn__For_Loop_Basic1.py
@@ -0,0 +1,37 @@
+# 1.
+# S
+
+# 2.
+# for x in range(5, 1001):
+#     if x % 5 == 0:
+#         print(x)
+
+# 3.
+# for x in range (1, 101):
+#     if x % 10 == 0:
+#         print('Coding Dojo')
+#     elif x % 5 == 0:
+#         print('Coding')
+#     else:
+#         print(x)
+
+# 4.
+# sum = 0
+# for x in range(0, 500001):
+#     if x % 2 != 0:
+#         sum += x
+# print(sum)
+
+# 5.
+# for x in range(2018, 1, -4):
+#     print(x)
+
+# 6.
+
+lowNum = 1
+highNum = 30
+mult = 5
+for x in range(lowNum, highNum):
+    if x % mult == 0:
+        print(x)
+
